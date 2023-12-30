7b9163e6052ee0622765cd10b08ab44cd7695117
Azamat <azamatkhabibullaevcto@gmail.com>
2023-12-31 00:30:58 +0500

    Run prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles/index.scss'
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./styles/index.scss"
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"))
 
 root.render(<App />)
